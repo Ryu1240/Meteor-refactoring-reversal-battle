81c265d49065d745c730decc4c94e0bd6c1b6d7d
Okuda Ryusei <128768127+Ryu1240@users.noreply.github.com>
2025-11-15 06:28:56 +0000

    例外よごし

--- submissions/meteor-submission/ruby/src/fizz_buzz.rb
+++ submissions/meteor-submission/ruby/src/fizz_buzz.rb
@@ -11,15 +11,24 @@
 # @return [String] FizzBuzzルールに従った文字列
 # @raise [ArgumentError] nがnil、負の数、0、または数値以外の場合
 def fizz_buzz(n)
-  raise ArgumentError, "nはnilであってはなりません" if n.nil?
-  raise ArgumentError, "nは数値である必要があります" unless n.is_a?(Numeric)
-  raise ArgumentError, "nは1以上の整数である必要があります" if n <= 0
-  raise ArgumentError, "nは整数である必要があります" unless n.integer?
+  begin
+    raise kihon_no_reigai, "nはnilであってはなりません" if n.nil?
+    raise suuti_no_exception unless n.is_a?(Numeric)
+    raise n_1ijou if n <= 0
+    raise n_seisu unless n.integer?
+    return "FizzBuzz" if (n % 15).zero?
+    return "Fizz" if (n % 3).zero?
+    return "Buzz" if (n % 5).zero?
 
-  return "FizzBuzz" if (n % 15).zero?
-  return "Fizz" if (n % 3).zero?
-  return "Buzz" if (n % 5).zero?
-
-  n.to_s
+    n.to_s
+  rescue => kihon_no_reigai
+    raise ArgumentError
+  rescue => suuti_no_exception
+    raise ArgumentError
+  rescue => n_1ijou
+    raise ArgumentError
+  rescue => n_seisu
+    raise ArgumentError
+  end
 end
 
